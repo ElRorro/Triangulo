66002fa6f4cf71625f8a9c245f885a438e41841f
Jesus Rodriguez <[juravideos@gmail.com]>
2016-11-30 13:49:50 +0100

    modificado:    MetodoArea.java 	modificado:    Triangulo.java

--- MetodoArea.java
+++ MetodoArea.java
@@ -1,13 +1,14 @@
-//Cacular altura ????
-
 public class MetodoArea{
 	
-	public static double calculaArea (double area, double baseTriangulo, double ladoTriangulo, double altura){
-	 baseTriangulo = ladoTriangulo/2;
-	 area = (baseTriangulo * calcularAltura(altura, ladoTriangulo, baseTriangulo))/2;
+	public static double calcularPerimetro (double ladoTriangulo){
+		return 3*ladoTriangulo;
+	}
+	
+	public static double calculaArea (double ladoTriangulo){
+	 double altura = Math.sqrt(Math.pow(ladoTriangulo, 2) - Math.pow(ladoTriangulo/2, 2) );
+	 double area = (ladoTriangulo * altura)/2;
 	 
 	 return area;
-	 
 	}
 	
     //La fórmula es a = (b * h) / 2
--- Triangulo.java
+++ Triangulo.java
@@ -2,29 +2,18 @@ import java.util.Scanner;
 
 public class Triangulo {
 	
-	public static double calcularPerimetro (double ladoTriangulo){
-		return 3*ladoTriangulo;
-	}
-	
-	public static double calcularAltura(double altura, double ladoTriangulo, double baseTriangulo){
-		baseTriangulo = ladoTriangulo/2;
-		altura = Math.sqrt(baseTriangulo*baseTriangulo + ladoTriangulo*ladoTriangulo);
-		
-		return altura;
-	}
-	
 	public static void main (String args[]) {
-		double perimetro;
-		double ladoTriangulo;
-		double altura = 0;
-		double baseTriangulo = 0;
+		double ladoTriangulo = 0;
+		
 		Scanner teclado = new Scanner(System.in);
 		
 		System.out.println("Introduce un lado para el triangulo: ");
 		ladoTriangulo = teclado.nextDouble();
+			
+		System.out.println("El perimetro del triangulo es " + MetodoArea.calcularPerimetro(ladoTriangulo));
 		
-		System.out.println("El perimetro del triangulo es " + calcularPerimetro(ladoTriangulo));
-		calcularAltura(altura, ladoTriangulo, baseTriangulo);
+		
+		System.out.println("El area del triangulo es " + MetodoArea.calculaArea(ladoTriangulo));
 		
 	}
 }
